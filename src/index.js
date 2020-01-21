ceea7953435666a514b7aa2de14b0a81b25d0727
portfolio-yh <a411684a@yahoo.co.jp>
2020-01-21 16:16:04 +0900

    Revert "no message"
    This reverts commit f42e78bf5953debface0ddcb7fd2a1a023a7ce4f.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
-import jQuery from 'jquery';
+// //npmを使ってインストール
+// npm i -D webpack webpack-cli
+//
+// i  ⇒ installの略
+// -D ⇒ インストール先をdevDependenciesにするための指定
 
-// ドルマークに参照を代入(慣習的な $ を使うため)
-const $ = jQuery;
+// import 文を使って sub.js ファイルを読み込む。
+import { hello } from "./sub";
+hello();
 
-// テキストを取得
-const text = $('#myText').text();
-$('#myText')
-    .empty() // 一旦、空にする
-    .show(); // 表示する
-
-const arr = text.split(''); // 一文字ずつ、配列に格納
-const elements = [];
-
-// 一文字ずつ、spanタグで包む
-arr.map((str, index) => {
-    elements[index] = $(`<span>${str}</span>`);
-    $('#myText').append(elements[index]); // 元の場所に挿入
-});
-
-// エフェクトの適用
-elements.map((element, index) => {
-    element
-        .delay(40 * index)
-        .queue(function () {
-            $(this).addClass('motion');
-        });
-});
+//require(node.js)を使って sub.js ファイルを読み込む。
+// const item = require('./sub.js');
+// item.hello();
