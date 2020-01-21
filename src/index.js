8f88250bd5a42d0c95cd34d1870beac0510813c2
portfolio-yh <a411684a@yahoo.co.jp>
2020-01-21 13:40:36 +0900

    webpackでローカルサーバーを起動し、変更時にブラウザをリロードする
    毎回ビルドコマンドをコマンドラインで打ち込むのは効率的でありません。ファイルの変更を検知し（watchともいいます）、自動的にビルドコマンドを実行し、ブラウザをリロードする・・・といった手順を自動化できます。類似の技術として「lite-server」や「BrowserSync」といったものがありますが、それに近いものだと考えていいでしょう。

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@
 
 // import 文を使って sub.js ファイルを読み込む。
 import { hello } from "./sub";
-
-// sub.jsに定義されたJavaScriptを実行する。
 hello();
+
+//require(node.js)を使って sub.js ファイルを読み込む。
+// const item = require('./sub.js');
+// item.hello();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    // webpack.config.jsファイルを用意することで、webpackの挙動を調整できます。
+    // よく使う設定として、エントリーポイントを指定するentryと、出力フォルダーをカスタマイズするoutputがあります。
+    // 必須ではないもの、よく使うオプションのため必ずおさえておきましょう。次のように指定します。
+
     // メインとなるJavaScriptファイル（エントリーポイント）
     entry: `./src/index.js`,
 
@@ -8,5 +12,21 @@ module.exports = {
         path: `${__dirname}/dist`,
         // 出力ファイル名
         filename: "main.js"
+    },
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: "development",
+
+    // ローカル開発用環境を立ち上げる
+    // 実行時にブラウザが自動的に localhost を開く
+    // npm run startで起動
+    devServer: {
+        contentBase: "dist",
+        open: true
     }
+
+
+
+
+
 };
